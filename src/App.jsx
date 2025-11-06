5e6aed7a7912e0b1a459135563815947da50323a
Mafer <maria.moya@tecsup.edu.pe>
2025-11-06 00:34:36 -0500

    Agregando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import "./index.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
-      <nav className="navbar">
+      <Navbar className="navbar">
         <h1>FakeStore Catalog</h1>
         <div>
           <Link to="/">Home</Link>
           <Link to="/products">Products</Link>
         </div>
-      </nav>
+      </Navbar>
 
       <Routes>
         <Route path="/" element={<Home />} />
